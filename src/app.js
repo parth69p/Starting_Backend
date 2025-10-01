1b43ab6bcb1623358294a7a5a8e5d9e8af32a0b5
parth <parth@DESKTOP-31NEM17>
2025-10-01 14:18:08 +0530

    added congigrations

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import Cors from "cors"
+import cookieParser from "cookie-parser";
+const app = express();
+// need to check this option. 
+app.use(Cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+// configrations. 
+app.use(express.json({limit:"16kb"})) // used to set limit of accepting data.
+app.use(express.urlencoded({extended:true,limit:"16kb"}))// this is used to search from url like : parthtinna = parth+tinna // parth%20tinna
+app.use(express.static("public"))// serving the static files from the server.
+app.use(cookieParser())
+export {app}
