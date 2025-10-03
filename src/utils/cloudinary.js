c7611b8ff390d05421c667f0677ac9e547d1ad5a
parth <parth@DESKTOP-31NEM17>
2025-10-03 19:55:21 +0530

    File upload using multer.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from "cloudinary"; 
+
+import fs from "fs";// file system. for file handling.
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        // upload file on Cloudinary 
+     const response =  await  cloudinary.uploader.upload(localFilePath,{// for uploading to cloudinary
+            resource_type:"auto"
+        })
+        // file has been uploaded successfull
+        console.log("File is uploaded on cloudinary ",response.url);
+        return response //
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)// remove the Locally saved temporary file as the upload operation got failed.
+        return null;
+    }
+}
+
+
+
+
+// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commans/a/ae/Olympic_falg.jpg",
+//     {public_id : "olymipic_flag"},
+//     function(error,result){console.log(result);
+//     }
+// )
+
+export {uploadOnCloudinary}
