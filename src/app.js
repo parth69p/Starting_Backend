659d7b2d22eea575433806a7ed2bd02ad354a241
parth <parth@DESKTOP-31NEM17>
2025-10-04 15:17:04 +0530

    user Controller and user Routes. are created (need more changes)

--- src/app.js
+++ src/app.js
@@ -12,4 +12,15 @@ app.use(express.json({limit:"16kb"})) // used to set limit of accepting data.
 app.use(express.urlencoded({extended:true,limit:"16kb"}))// this is used to search from url like : parthtinna = parth+tinna // parth%20tinna
 app.use(express.static("public"))// serving the static files from the server.
 app.use(cookieParser())
+
+
+// routes import 
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
 export {app}
