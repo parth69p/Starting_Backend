798cf53e4d2d54114d7e79994f276acf5f69c63a
parth <parth@DESKTOP-31NEM17>
2025-10-07 19:31:28 +0530

    minor changes

--- src/Db/index.js
+++ src/Db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance}`);
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.name}`);
   } catch (error) {
     console.log("MONGODB Connection error");
     process.exit(1);
